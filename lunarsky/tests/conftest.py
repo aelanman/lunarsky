f44c63ecf4dceefb469c70693466741d7faf70f1
Adam Lanman <adam_lanman@brown.edu>
2020-01-11 00:52:21 -0500

    ignore a warning in tests

---
+++ lunarsky/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+import warnings
+
+# Ignore a deprecation from spiceypy
+@pytest.fixture(autouse=True)
+def representation_deprecation_to_error():
+    warnings.filterwarnings('ignore', message='Using or importing the ABCs from'
+                                              '\'collections\' instead of from'
+                                              '\'collections.abc\' is deprecated'
+                                              
+                                    , category=DeprecationWarning)
+
